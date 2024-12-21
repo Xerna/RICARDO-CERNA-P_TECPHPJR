b18e087438d35dd0a96102bee96aeb76ea5fc7b8
Xerna <ricardo.cerna2001@gmail.com>
2024-12-20 19:48:54 -0600

    fix: se soluciono lenguaje de la tabla

--- public/js/usuarios.js
+++ public/js/usuarios.js
@@ -45,7 +45,26 @@ class UsuariosManager {
             ],
             order: [[0, 'desc']],
             language: {
-                url: 'https://cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json'
+                processing: "Procesando...",
+                search: "Buscar:",
+                lengthMenu: "Mostrar _MENU_ registros",
+                info: "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                infoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+                infoFiltered: "(filtrado de un total de _MAX_ registros)",
+                infoPostFix: "",
+                loadingRecords: "Cargando...",
+                zeroRecords: "No se encontraron resultados",
+                emptyTable: "Ningún dato disponible en esta tabla",
+                paginate: {
+                    first: "Primero",
+                    previous: "Anterior",
+                    next: "Siguiente",
+                    last: "Último"
+                },
+                aria: {
+                    sortAscending: ": Activar para ordenar la columna de manera ascendente",
+                    sortDescending: ": Activar para ordenar la columna de manera descendente"
+                }
             },
             scrollY: '50vh',
             scroller: true,
